9a316ac0e0d6186461ae2da78aab9143212edf53
leobaiao <leobaiao966@gmail.com>
2025-10-12 16:13:44 -0300

    fix template

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 // app/templates/page.tsx
 
 import { createClient } from "@/lib/supabase/server"; // <-- Mude aqui
 import { redirect } from "next/navigation";
 import TemplatesClient from "@/components/templates-client";
-=======
-import { createServerClient } from "@/lib/supabase/server"
-import { redirect } from "next/navigation"
-import TemplatesClient from "@/components/templates-client"
 export const dynamic = 'force-dynamic'
->>>>>>> cc2f3f157633c8e4eef4c0b91f50853fd2d91ddf
-
 export default async function TemplatesPage() {
   const supabase = await createClient(); // <-- E aqui
 
