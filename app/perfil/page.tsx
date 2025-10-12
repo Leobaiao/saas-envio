1f0bb1aade0afde8fcd2a9ac6309f2bd86404090
Leonardo Baião <143014306+Leobaiao@users.noreply.github.com>
2025-10-12 14:48:13 -0300

    Update page.tsx

--- app/perfil/page.tsx
+++ app/perfil/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation"
 import { createServerClient } from "@/lib/supabase/server"
 import PerfilClient from "@/components/perfil-client"
-
+export const dynamic = 'force-dynamic'
 export default async function PerfilPage() {
   const supabase = await createServerClient()
 
